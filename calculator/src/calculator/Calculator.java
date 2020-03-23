3819db22389d5f049d08f182052730bc1c56fbc5
craft software <contact@icraftsoft.com>
2020-03-23 18:46:40 -0400

    first testng file

---
+++ calculator/src/calculator/Calculator.java
@@ -0,0 +1,29 @@
+package calculator;
+
+public class Calculator {
+
+	public int sum(int param1, int param2) {
+		return param1 + param2;
+
+	}
+
+	public int multiply(int param1, int param2) {
+		return param1 * param2;
+
+	}
+
+	public int divide(int param1, int param2) {
+		return param1 / param2;
+
+	}
+
+	public int substract(int param1, int param2) {
+		return param1 - param2;
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ calculator/src/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CalculatorTest {
+	
+  Calculator calculator =  new Calculator();
+  
+
+  @Test
+  public void multiplyTest() {
+    
+    Assert.assertEquals(calculator.multiply(2, 2), 4);
+  }
+  
+  @Test
+  public void sumTest() {
+	Assert.assertEquals(calculator.sum(3, 3), 6);
+  }
+  
+  @Test
+  public void subtractTest() {
+	Assert.assertEquals(calculator.substract(3, 3), 0);
+  }
+  
+  @Test
+  public void divideTest() {
+	Assert.assertEquals(calculator.divide(3, 3), 1);
+  }
+}
