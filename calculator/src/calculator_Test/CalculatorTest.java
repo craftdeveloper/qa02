596c7398be70b586657d82141d0cfaf80669fe6b
craft software <contact@icraftsoft.com>
2020-03-24 16:20:30 -0400

    updated test methods

--- calculator/src/calculator_Test/CalculatorTest.java
+++ calculator/src/calculator_Test/CalculatorTest.java
@@ -1,34 +1,44 @@
-package calculator;
+package calculator_Test;
 
 import org.testng.annotations.Test;
 
 import calculator_Impl.Calculator;
+import calculator_Int.ICalculator;
 
 import org.testng.Assert;
 
+
 public class CalculatorTest {
 	
-  Calculator calculator =  new Calculator();
+  ICalculator calculator =  new Calculator();
   
 
-  @Test
+  @Test (groups = {"functional"} )
+
   public void multiplyTest() {
     
     Assert.assertEquals(calculator.multiply(2, 2), 4);
   }
   
-  @Test
+  @Test (groups = {"functional"} )
   public void sumTest() {
 	Assert.assertEquals(calculator.sum(3, 3), 6);
   }
   
-  @Test
+  @Test (groups = {"non functional"} )
   public void subtractTest() {
 	Assert.assertEquals(calculator.substract(3, 3), 0);
   }
   
-  @Test
+  @Test (groups = {"non functional"} )
   public void divideTest() {
 	Assert.assertEquals(calculator.divide(3, 3), 1);
+  }
+  
+  @Test (dependsOnMethods = {"sumTest", "multiplyTest"})
+  public void a_sumMultiply () {
+
+	  Assert.assertEquals(calculator.multiply(2, 2), 4);
+	  
   }
 }
