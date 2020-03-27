f6ee5a9323e5b73f8a0e7f79ba8bfcb2396cc3ce
bethani <wendifraw15@gmail.com>
2020-03-26 19:51:31 -0700

    mack2 change

---
+++ calculator/src/calculator_Test/CalculatorTest56.java
@@ -0,0 +1,45 @@
+package calculator_Test;
+
+import org.testng.annotations.Test;
+
+import calculator_Imp.Calculator;
+import calculator_Int.ICalculator;
+
+import org.testng.Assert;
+
+
+public class CalculatorTest56 {
+	
+  ICalculator calculator =  new Calculator();
+  
+
+  @Test (groups = {"functional"} )
+
+  public void multiplyTest() {
+    
+    Assert.assertEquals(calculator.multiply(2, 2), 4);
+  }
+  
+  @Test (groups = {"functional"} )
+  public void sumTest() {
+	Assert.assertEquals(calculator.sum(3, 3), 6);
+  }
+  
+  @Test (groups = {"non functional"} )
+  public void subtractTest() {
+	Assert.assertEquals(calculator.substract(3, 3), 0);
+  }
+  
+  @Test (groups = {"non functional"} )
+  public void divideTest() {
+	Assert.assertEquals(calculator.divide(3, 3), 1);
+  }
+  
+  @Test (dependsOnMethods = {"sumTest", "multiplyTest"})
+  public void a_sumMultiply () {
+
+	  Assert.assertEquals(calculator.multiply(2, 2), 4);}
+	  
+	  
+	 
+}
